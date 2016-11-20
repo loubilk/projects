4f7f0d80fe9393839416ddea6bc6cc3af3355f42
Jiri Mencak <jmencak@redhat.com>
2016-11-20 22:11:37 +0100

    more flexibility to cluster loading scripts

--- haproxy/bin/bench.sh
+++ haproxy/bin/bench.sh
@@ -4,20 +4,21 @@ ProgramName=${0##*/}
 
 routes=$(printf "%04d" $(oc get routes --all-namespaces | grep hello- | wc -l))
 gotime_sh=/root/projects/haproxy/bin/gotime.sh
+ppr=1			# pods per route (replicas)
 use_pbench=Y
-export RUN_TIME=60	# TODO, run time in seconds
+export RUN_TIME=600	# benchmark run-time in seconds
 export VEGETA_RPS	# client requests per second
 
-#for VEGETA_RPS in 1000 2000 3000 4000; do
-for VEGETA_RPS in 3000 ; do
+for VEGETA_RPS in 2000 3000 4000; do
+#for VEGETA_RPS in 9000; do
   rps_f=$(printf "%04d" $VEGETA_RPS)
 
   if test "$use_pbench" = Y ; then
-    pbench-user-benchmark -C haproxy-r${routes}-${rps_f}rps-${RUN_TIME}s-hello-openshift -- $gotime_sh
+    pbench-user-benchmark -C haproxy-${routes}r-${ppr}ppr-${rps_f}rps-${RUN_TIME}s-hello-openshift -- $gotime_sh
   else
     $gotime_sh
   fi
-  echo "sleeping 600"
-  sleep 600
+  echo "sleeping 60"
+  sleep 60
 done
 
--- haproxy/bin/scale.sh
+++ haproxy/bin/scale.sh
@@ -4,9 +4,9 @@ cluster_loader_dir=/root/svt/openshift_scalability
 routes=$(printf "%04d" $(oc get routes --all-namespaces | grep hello-openshift | wc -l))
 template=../apps/hello-openshift/hello-openshift.json
 project_basename=hello-openshift-
-projects=180	# 10, 20, 30, 60, 180
-project_start=125
+projects=60	# 10, 20, 30, 60, 180
 templates=10	# templates per project
+replicas=3
 
 max_not_running() {
   local max_not_running="${1:-10}"
@@ -21,38 +21,22 @@ max_not_running() {
   done
 }
 
-new_project_or_reuse() {
-  local project="$1"
-
-  if oc project $project 2>&1 ; then
-    # $project exists, recycle it
-    for res in rc dc bc pod service route ; do
-      oc delete $res --all -n $project 2>&1
-    done
-  else
-    # $project doesn't exist
-    oc new-project $project
-  fi
-}
-
 main() {
   local project
-  local data=$(printf "%1024s"|tr ' ' '.')	# data to send back from hello pods
 
-  for p in $(seq $project_start $projects)
+  for p in $(seq 1 $projects)
   do
     project=${project_basename}$p
-    new_project_or_reuse $project
     i=1
     while test $i -le $templates
     do
-      oc process -vIDENTIFIER=$i -vREPLICAS=1 -vRESPONSE="$data" -f $template | \
-        oc create -f- -n $project
+      oc scale rc ${project_basename}$i -n ${project_basename}$p --replicas=${replicas}
       i=$((i+1))
       
-      max_not_running 5
+      max_not_running 10
     done
   done
 }
 
 main
+
